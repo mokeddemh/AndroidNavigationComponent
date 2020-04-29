0e0a472401a14281c1b56fd03f3c3b3767d8d3ad
hakim <h_mokeddem@esi.dz>
2020-04-29 09:02:59 +0100

    Navigation Drawer modified

--- app/src/main/java/com/android/example/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/android/example/navigationcomponent/MainActivity.kt
@@ -5,16 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import kotlinx.android.synthetic.main.activity_main2.*
+import kotlinx.android.synthetic.main.activity_main.*
 
-class Main2Activity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
     lateinit var navController:NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        setContentView(R.layout.activity_main)
         navController = findNavController(R.id.navHostFragment)
-        setSupportActionBar(toolbar)
         NavigationUI.setupActionBarWithNavController(this,navController,drawerLayout)
         NavigationUI.setupWithNavController(nav_view,navController)
     }
--- app/src/main/java/com/android/example/navigationcomponent/MainFragment.kt
+++ app/src/main/java/com/android/example/navigationcomponent/MainFragment.kt
@@ -6,9 +6,7 @@ import android.view.*
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
-import androidx.recyclerview.widget.LinearLayoutManager
-import entity.City
-import kotlinx.android.synthetic.main.fragment_main.*
+
 
 
 class MainFragment : Fragment() {
@@ -24,28 +22,10 @@ class MainFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        recyclerView.layoutManager = LinearLayoutManager(activity!!)
-        recyclerView.adapter = CityAdapter(activity!!,loadData())
         setHasOptionsMenu(true)
 
     }
 
-
-    fun loadData():List<City> {
-
-        //val detailImages = arrayOf(R.drawable.paris_detail,R.drawable.london_detail,R.drawable.barcelone_detail,R.drawable.istanbul_detail,R.drawable.rome_detail)
-        val namesTab = resources.getStringArray(R.array.cities)
-        val touristsTab = resources.getStringArray(R.array.tourists)
-        //val placesTab = resources.getStringArray(R.array.places)
-        //val descTab = resources.getStringArray(R.array.description)
-        val imagesTab = arrayOf(R.drawable.paris_list,R.drawable.london_list,R.drawable.barcelona_list,R.drawable.istanbul_list,R.drawable.roma_list)
-        val list = mutableListOf<City>()
-        for (i in 0..imagesTab.size-1) {
-            list.add(City(listImage = imagesTab[i], name = namesTab[i], touristNumber = touristsTab[i]))
-
-        }
-        return  list
-    }
 
 
 
